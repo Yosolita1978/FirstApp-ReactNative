55627674c529e6227cc3f038b2a357c51f11dadb
cristina <yosola@gmail.com>
2021-02-16 14:01:25 -0800

    Hello Word Message and button working

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [outputText, setOutoutText] = useState("Terf la que lo lea")
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{outputText}</Text>
+      <Button title="Change Text" onPress={() => setOutoutText("The text changes, my mind doesn't")}/>
       <StatusBar style="auto" />
     </View>
   );
